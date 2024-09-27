98b282d19843208fdbd05f27aec2fd99331fb82f
ascc-br <alfran.carvalho@outlook.com>
2024-09-26 21:19:42 -0300

    Swap Players & New factorings

--- script.js
+++ script.js
@@ -1,204 +1,282 @@
 class User {
-    constructor(name) {
-        this.name = name;
-        // this.age = age;     // Atributo opcional
-        // this.score = score; // Atributo opcional
-    }
+  constructor(name) {
+    this.name = name;
+    // this.age = age;     // Atributo opcional
+    // this.score = score; // Atributo opcional
+  }
 }
+
+var userList = [];
 
 class PlayersManager {
-    constructor() {
-        this.userList = [];  // Lista de objetos User
-        this.team_size = 4;
-        this.NaProxima = 0;
-    }
+  constructor() {
+    this.team_size = 4;
+    this.NaProxima = 0;
+  }
 
-    // Função para inserir um novo usuários na lista
-    addUser(name) {
-        const user = new User(name);
-        if (document.getElementById("BttnToNaPr").classList.contains("glowing")) {
-            this.userList.splice(this.NaProxima, 0, user);
-            this.NaProxima++;
-        } else {
-            this.userList.push(user);
-        }
-        this.updateNameTable();
-    }
-
-    // Atualiza a tabela com a lista de nomes
-    updateNameTable() {
-        const tableBody = document.getElementById('nameTable').getElementsByTagName('tbody')[0];
-        if (this.userList.length > 0)
-            document.getElementById("nameTable").classList.remove("hidden");
-        else
-            document.getElementById("nameTable").classList.add("hidden");
-        tableBody.innerHTML = ''; //limpa a tabela
-
-        let order = 1; //reinicia a ordem
-        this.userList.forEach((user, index) => {
-            const row = tableBody.insertRow();
-
-            const nameCell = row.insertCell(0);
-            const orderCell = row.insertCell(1);
-            const actionsCell = row.insertCell(2);
-            actionsCell.classList.add('actions-cell'); //adiciona a classe para aplicar o CSS
-
-            nameCell.textContent = user.name;
-            orderCell.textContent = order++;
-
-            const delButton = document.createElement('button');
-            delButton.textContent = 'del';
-            delButton.classList.add('action-btn', 'del');
-            delButton.addEventListener('click', () => {
-                this.userList.splice(index, 1);
-                this.updateNameTable();
-            });
-
-            const upButton = document.createElement('button');
-            upButton.textContent = '▲';
-            upButton.classList.add('action-btn', 'up');
-            upButton.addEventListener('click', () => this.moveRow(index, -1));
-
-            const downButton = document.createElement('button');
-            downButton.textContent = '▼';
-            downButton.classList.add('action-btn', 'down');
-            downButton.addEventListener('click', () => this.moveRow(index, 1));
-
-            actionsCell.appendChild(delButton);
-            actionsCell.appendChild(upButton);
-            actionsCell.appendChild(downButton);
-        });
-    }
-
-    // Função para mover um nome na lista (cima ou baixo)
-    moveRow(index, direction) {
-        const newIndex = index + direction;
-        if (newIndex >= 0 && newIndex < this.userList.length) {
-            const temp = this.userList[index];
-            this.userList[index] = this.userList[newIndex];
-            this.userList[newIndex] = temp;
-            this.updateNameTable();
-        }
-    }
-
-    //função para retorna apenas a lista de nomes
-    getNamesList() {
-        return this.userList.map(jogadores => jogadores.name);
-    }
-}
-
-const usersManager = new PlayersManager();
-
-// Função que lida com o input e passa os dados para a classe
-function addNames () {
-    const nameInput = document.getElementById('nameInput').value.trim();
-
-    // Verifica se há nomes
-    if (nameInput) {
-        // Divide a string de nomes por vírgula e remove espaços em branco de cada nome
-        const namesArray = nameInput.split(',').map(name => name.trim());
-
-        // Para cada nome, adiciona um novo usuário
-        namesArray.forEach(name => {
-            if (name) { // Garante que o nome não esteja vazio
-                usersManager.addUser(name); // Chama a função da classe PlayersManager
-            }
-        });
-
-        document.getElementById('nameInput').value = '';  // Limpa o input após adicionar os nomes
-        usersManager.updateNameTable();  // Atualiza a tabela
-    }
-}
-
-//função de clique ou apertar enter
-document.getElementById('addButton').addEventListener('click', addNames);
-document.getElementById("nameInput").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") { // Verifica se a tecla pressionada é "Enter"
-        addNames(); // Chama a função como se fosse um clique no botão
-    }
-});
-
-//Função ToNaProxima (muda a prioridade dos nomes inseridos)
-document.getElementById("BttnToNaPr").addEventListener("click", function() {
-    this.classList.toggle("glowing"); // Alterna a classe 'glowing' ao clicar
-});
-
-// Função para mudar tamanho das equipes
-document.getElementById('team_size_button').addEventListener('click', function() {
-    const teamSizeInput = document.getElementById('teamSizeBox');
-    const newTeamSize = parseInt(teamSizeInput.value);
-    if (!isNaN(newTeamSize) && newTeamSize > 0) {
-        usersManager.team_size = newTeamSize;
-        usersManager.updateNameTable(); //verificar se é realmente necessário chamar essa função
-    }
-});
-
-// Função para alternar entre as telas
-function toggleScreens() {
-    const screen1 = document.getElementById('screen1');
-    const screen2 = document.getElementById('screen2');
-    screen1.style.display = screen1.style.display === 'none' ? 'block' : 'none';
-    screen2.style.display = screen2.style.display === 'none' ? 'block' : 'none';
-}
-
-// Função para copiar a lista para o clipboard
-function controlceh () {
-    navigator.clipboard.writeText(usersManager.getNamesList()).then(() => {
-        alert('Lista de nomes copiada para o clipboard!');
-    }).catch(err => {
-        console.error('Erro ao copiar: ', err);
-    });
-}
-
-// Função para gerar times
-document.getElementById('generateTeamsButton').addEventListener('click', function() {
-    const team_size = usersManager.team_size;
-
-    if (usersManager.userList.length < team_size * 2) {
-        alert('São necessários ao menos ' + team_size * 2 + ' nomes para gerar times!');
+  // Função para inserir um novo usuários na lista
+  addUser(name) {
+    const user = new User(name);
+    if (document.getElementById("BttnToNaPr").classList.contains("glowing")) {
+      userList.splice(this.NaProxima, 0, user);
+      this.NaProxima++;
     } else {
-        controlceh();
+      userList.push(user);
+    }
+    this.updateNameTable();
+  }
 
-        const team1List = document.getElementById('team1List');
-        const team2List = document.getElementById('team2List');
-        // Limpa as listas antes de gerar os times
-        team1List.innerHTML = '';
-        team2List.innerHTML = '';
+  // Atualiza a tabela com a lista de nomes
+  updateNameTable() {
+    const tableBody = document
+      .getElementById("nameTable")
+      .getElementsByTagName("tbody")[0];
+    if (userList.length > 0)
+      document.getElementById("nameTable").classList.remove("hidden");
+    else document.getElementById("nameTable").classList.add("hidden");
+    tableBody.innerHTML = ""; //limpa a tabela
 
-        // Divide os primeiros nomes em 2 times
-        const team1 = usersManager.getNamesList().slice(0, team_size);
-        const team2 = usersManager.getNamesList().slice(team_size, team_size * 2);
+    let order = 1; //reinicia a ordem
+    userList.forEach((user, index) => {
+      const row = tableBody.insertRow();
 
-        // Exibe os nomes nos times
-        team1.forEach(name => {
-            const li = document.createElement('li');
-            li.textContent = name;
-            team1List.appendChild(li);
-        });
+      const nameCell = row.insertCell(0);
+      const orderCell = row.insertCell(1);
+      const actionsCell = row.insertCell(2);
+      actionsCell.classList.add("actions-cell"); //adiciona a classe para aplicar o CSS
 
-        team2.forEach(name => {
-            const li = document.createElement('li');
-            li.textContent = name;
-            team2List.appendChild(li);
-        });
+      nameCell.textContent = user.name;
+      orderCell.textContent = order++;
 
-        toggleScreens(); // Muda para a segunda tela
-    } 
-});
+      const delButton = document.createElement("button");
+      delButton.textContent = "del";
+      delButton.classList.add("action-btn", "del");
+      delButton.addEventListener("click", () => {
+        userList.splice(index, 1);
+        this.updateNameTable();
+      });
 
-// Função para jogar (volta para a primeira tela e coloca os nomes no final da lista)
-document.getElementById('playButton').addEventListener('click', function() {
-    const movedNames = usersManager.userList.splice(0, usersManager.team_size * 2); // Remove os nomes dos que jogaram
-    usersManager.userList.push(...movedNames); // Adiciona os nomes removidos ao final
+      const upButton = document.createElement("button");
+      upButton.textContent = "▲";
+      upButton.classList.add("action-btn", "up");
+      upButton.addEventListener("click", () => this.moveRow(index, -1));
+
+      const downButton = document.createElement("button");
+      downButton.textContent = "▼";
+      downButton.classList.add("action-btn", "down");
+      downButton.addEventListener("click", () => this.moveRow(index, 1));
+
+      actionsCell.appendChild(delButton);
+      actionsCell.appendChild(upButton);
+      actionsCell.appendChild(downButton);
+    });
+  }
+
+  // Função para mover um nome na lista (cima ou baixo)
+  moveRow(index, direction) {
+    const newIndex = index + direction;
+    if (newIndex >= 0 && newIndex < userList.length) {
+      const temp = userList[index];
+      userList[index] = userList[newIndex];
+      userList[newIndex] = temp;
+      this.updateNameTable();
+    }
+  }
+
+  //função para retorna apenas a lista de nomes
+  getNamesList() {
+    return userList.map((jogadores) => jogadores.name);
+  }
+}
+
+class TeamManager {
+  constructor() {
+    this.team1 = [];
+    this.team2 = [];
+  }
+
+  // Método para preencher e atualizar as listas de times
+  fillAndUpdateLists(userList, team_size) {
+    const team1List = document.getElementById("team1List");
+    const team2List = document.getElementById("team2List");
+
+    // Limpa as listas antes de gerar os times
+    team1List.innerHTML = "";
+    team2List.innerHTML = "";
+
+    // Divide os primeiros nomes em 2 times
+    this.team1 = userList.slice(0, team_size);
+    this.team2 = userList.slice(team_size, team_size * 2);
+
+    // Exibe os nomes nos times
+    this.displayTeamMembers(this.team1, team1List, "team1radio");
+    this.displayTeamMembers(this.team2, team2List, "team2radio");
+  }
+
+  // Método para exibir os membros do time
+  displayTeamMembers(team, listElement, radioName) {
+    team.forEach((name) => {
+      const li = document.createElement("li");
+      const radio = document.createElement("input");
+
+      radio.type = "radio"; // Define o tipo como radio
+      radio.name = radioName; // Nome do grupo de radio buttons
+      radio.value = name; // O valor do radio button é o nome do jogador
+
+      // Adiciona o radio button e o nome ao li
+      li.appendChild(radio);
+      li.appendChild(document.createTextNode(name));
+
+      // Adiciona o li à lista de times
+      listElement.appendChild(li);
+    });
+  }
+
+  // Método do botão jogar que adiciona os nomes ao final da userList
+  play(userList, team_size) {
+    const movedNames = userList.splice(0, team_size * 2); // Remove os nomes dos que jogaram
+    userList.push(...movedNames); // Adiciona os nomes removidos ao final
 
     // Atualiza a tabela na primeira tela
     usersManager.NaProxima = 0;
     usersManager.updateNameTable();
-    toggleScreens(); // Volta para a tela inicial
+  }
+
+  // Método do botão trocar de time
+  swapPlayers(radio1, radio2) {
+    const player1Name = radio1.value;
+    const player2Name = radio2.value;
+
+    const player1Index = this.team1.indexOf(player1Name);
+    const player2Index = this.team2.indexOf(player2Name);
+
+    // Se ambos os jogadores estiverem em times diferentes, troca
+    if (player1Index !== -1 && player2Index !== -1) {
+      // Troca os jogadores
+      this.team1[player1Index] = player2Name;
+      this.team2[player2Index] = player1Name;
+    }
+
+    // Atualiza as listas após a troca
+    const team1List = document.getElementById("team1List");
+    const team2List = document.getElementById("team2List");
+    this.fillAndUpdateLists([...this.team1, ...this.team2], this.team1.length);
+  }
+}
+// Função para alternar entre as telas
+function toggleScreens() {
+  const screen1 = document.getElementById("screen1");
+  const screen2 = document.getElementById("screen2");
+  screen1.style.display = screen1.style.display === "none" ? "block" : "none";
+  screen2.style.display = screen2.style.display === "none" ? "block" : "none";
+}
+
+/** -------------> FUNÇÕES DA PRIMEIRA TELA <------------- */
+const usersManager = new PlayersManager();
+
+// Função que lida com o input e passa os dados para a classe
+function addNames() {
+  const nameInput = document.getElementById("nameInput").value.trim();
+
+  // Verifica se há nomes
+  if (nameInput) {
+    // Divide a string de nomes por vírgula e remove espaços em branco de cada nome
+    const namesArray = nameInput.split(",").map((name) => name.trim());
+
+    // Para cada nome, adiciona um novo usuário
+    namesArray.forEach((name) => {
+      if (name) {
+        // Garante que o nome não esteja vazio
+        usersManager.addUser(name); // Chama a função da classe PlayersManager
+      }
+    });
+
+    document.getElementById("nameInput").value = ""; // Limpa o input após adicionar os nomes
+    usersManager.updateNameTable(); // Atualiza a tabela
+  }
+}
+
+//função de clique ou apertar enter
+document.getElementById("addButton").addEventListener("click", addNames);
+document
+  .getElementById("nameInput")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      // Verifica se a tecla pressionada é "Enter"
+      addNames(); // Chama a função como se fosse um clique no botão
+    }
+  });
+
+//Função ToNaProxima (muda a prioridade dos nomes inseridos)
+document.getElementById("BttnToNaPr").addEventListener("click", function () {
+  this.classList.toggle("glowing"); // Alterna a classe 'glowing' ao clicar
 });
 
-// Função para voltar sem jogar (somente volta para a tela inicial)
-document.getElementById('backButton').addEventListener('click', function() {
-    toggleScreens(); // Volta para a tela inicial
+// Função para mudar tamanho das equipes
+document
+  .getElementById("team_size_button")
+  .addEventListener("click", function () {
+    const teamSizeInput = document.getElementById("teamSizeBox");
+    const newTeamSize = parseInt(teamSizeInput.value);
+    if (!isNaN(newTeamSize) && newTeamSize > 0) {
+      usersManager.team_size = newTeamSize;
+      usersManager.updateNameTable(); //verificar se é realmente necessário chamar essa função
+    }
+  });
+
+// Função para copiar a lista para o clipboard
+function controlceh() {
+  navigator.clipboard
+    .writeText(usersManager.getNamesList())
+    .then(() => {
+      alert("Lista de nomes copiada para o clipboard!");
+    })
+    .catch((err) => {
+      console.error("Erro ao copiar: ", err);
+    });
+}
+
+/** -------------> FUNÇÕES DA SEGUNDA TELA <------------- */
+
+// Instancia a classe TeamManager
+const teamManager = new TeamManager();
+
+// Listener para gerar times
+document
+  .getElementById("generateTeamsButton")
+  .addEventListener("click", function () {
+    const team_size = usersManager.team_size;
+
+    if (userList.length < team_size * 2) {
+      alert(
+        "São necessários ao menos " + team_size * 2 + " nomes para gerar times!"
+      );
+    } else {
+      controlceh();
+      teamManager.fillAndUpdateLists(usersManager.getNamesList(), team_size);
+      toggleScreens(); // Muda para a segunda tela
+    }
+  });
+
+// Listener para jogar
+document.getElementById("playButton").addEventListener("click", function () {
+  teamManager.play(userList, usersManager.team_size);
+  toggleScreens(); // Volta para a tela inicial
 });
-    
+
+// Listener para voltar sem jogar
+document.getElementById("backButton").addEventListener("click", function () {
+  toggleScreens(); // Volta para a tela inicial
+});
+
+// Listener para trocar de time
+document.getElementById("swapButton").addEventListener("click", function () {
+  const radio1 = document.querySelector('input[name="team1radio"]:checked');
+  const radio2 = document.querySelector('input[name="team2radio"]:checked');
+
+  if (radio1 && radio2) {
+    teamManager.swapPlayers(radio1, radio2);
+  } else {
+    alert("Selecione um jogador de cada time para trocar!");
+  }
+});
